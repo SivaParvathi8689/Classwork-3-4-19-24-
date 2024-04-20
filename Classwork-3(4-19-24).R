c42cbb90d6500ac57165b9011db76046a07c8fe8
Ysiva@8689 <ysivaparvathi23@gmail.com>
2024-04-19 20:16:20 -0400

    first commit

---
+++ Classwork-3(4-19-24).R
@@ -0,0 +1,32 @@
+N <-100000
+a <-rnorm(N)
+b <- rnorm(N)
+#Vectorized approach
+c <- a * b
+c
+
+X <- rnorm(5)
+X
+X <- rnorm( 5, 10, 1)
+X
+X <- rnorm (5, 10)
+X
+X <- rnorm(5, mean=10, sd = 1)
+X
+X <- rnorm(5, sd=1, mean=10)
+X
+X <- rnorm(5, sd=1)
+X
+
+x <-rnorm(5, 10, 1)
+plot(x)
+hist(x)
+
+x <-rnorm(80000)
+plot(x)
+hist(x)
+head(x)
+tail(x)
+range(x)
+summary(x)
+boxplot(x)
